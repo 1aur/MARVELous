a43f144a13403e9a01a1b13fabf43f5519f3bd5d
Lauren Rodriguez <131307844+1aur@users.noreply.github.com>
2025-07-16 10:51:46 -0400

    Finished Homework

---
+++ MARVELous/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import MarvelInfo from './components/MarvelInfo';
+
+function App() {
+  return (
+    <div className="whole-page">
+      <MarvelInfo />
+    </div>
+  );
+}
+
+export default App;
